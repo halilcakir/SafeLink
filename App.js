01e4169cfc41655689192905f41a803c689db661
halil <halilcakir@Halil-MacBook-Air.local>
2025-02-11 11:56:40 +0300

    Signup sayfası değişiklikleri

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import Login from './screens/login';
+import signup from './screens/signup';
+import Signup from './screens/signup';
 
 
 export default function App() {
 
   return (
   
-    <Login/>
+    <Signup/>
   
   );
 }
---
+++ screens/signup.js
@@ -0,0 +1,107 @@
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+
+
+
+export default function Signup() {
+  return (
+    <View style={styles.container} >
+    <View style={styles.forms} >
+
+    <Text style={{fontSize:32, textAlign:'center', paddingBottom:25}}>SignUp</Text>
+
+    <View style={styles.inputContainer}>
+    
+      <Icon name="user" size={25} color="#888" style={styles.icon} />
+      <TextInput style={styles.textinput}
+    placeholder='Username'></TextInput>
+    </View>
+
+
+    <View style={styles.inputContainer}>
+        <Icon name='envelope' size={20} color="#888"></Icon>
+    <TextInput style={styles.textinput}
+         placeholder='Email'> 
+         </TextInput>
+    </View>
+   
+
+
+    <View style={styles.inputContainer}>
+    <Icon name='lock' size={25} color="#888" ></Icon>
+    <TextInput style={styles.textinput} placeholder='Password'></TextInput></View>
+    
+    <View style={styles.inputContainer}>
+    <Icon name='lock' size={25} color="#888" ></Icon>
+    <TextInput style={styles.textinput}placeholder='Confirm Password'></TextInput>
+    </View>
+    <View style={styles.PressableContainer}>
+    <Pressable style={styles.signupPressable}>
+        <Text>SignUp</Text>
+    </Pressable>
+    </View>
+    </View>
+      
+    </View>
+  )
+}
+
+const styles= StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:"center"
+    }
+    ,
+    forms:{
+     
+        width:'80%',
+        height:'400',
+        justifyContent:'space-evenly'
+
+    },
+    textinput:{
+       
+        height:50,
+        borderRadius:10,
+        textAlign:'center',
+        fontSize:20,
+        textAlign:'left',
+        paddingLeft:10
+
+
+        
+    },inputContainer: {
+        flexDirection: 'row', // Icon ve TextInput'u yatayda hizaladık
+        alignItems: 'center', // Dikeyde hizalama
+        borderColor: '#ccc',
+        paddingHorizontal: 10,
+        marginVertical: 10,
+        borderBottomWidth:1
+      },
+      icon: {
+        marginRight: 10, // Icon ile TextInput arasında boşluk
+      },
+      input: {
+        flex: 1, // TextInput'un genişliğini otomatik olarak ayarlayalım
+        height: 40,
+      },
+      signupPressable:{
+        backgroundColor:'#7886C7',
+        marginTop:50,
+        width:'40%',
+        alignItems:'center',
+        borderRadius:10,
+        height:50,
+        justifyContent:'center'
+
+      },PressableContainer:{
+        flex:1,
+        alignItems:'center',
+    
+      }
+
+})
